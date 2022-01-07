7a7c256f7b61c7e1e58a314e47c65a1006ea7791
Siddhartha Bhattacharjee <sidbhatt8888@gmail.com>
2022-01-07 10:04:18 +0530

    edited DATABASES

--- cowinclone/settings.py
+++ cowinclone/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'cowinclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB_name',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
