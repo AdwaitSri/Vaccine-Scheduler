b969cd0348e39a9521805f422406a5a1d8ce8bc8
Adw_Z_Coder <adwait.sri1702@gmail.com>
2022-01-09 00:03:54 +0530

    models

--- calc/models.py
+++ calc/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class VaccinationDetails(models.Model):
+    reg_id = models.IntegerField(db_column='Reg_id', primary_key=True)  # Field name made lowercase.
+    vaccine_count = models.IntegerField()
+    booking_status = models.SmallIntegerField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return(self.reg_id)
+
+
+class PersonalDetails(models.Model):
+    reg_id = models.AutoField(db_column='Reg_id', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(max_length=30)
+    phone_no = models.BigIntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    pwd = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return(self.name)
